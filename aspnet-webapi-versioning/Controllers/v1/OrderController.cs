b08f71472b3292fd89bfc37b27436d3f241660c3
Asif <muhammad.asif@thriftmetrics.com>
2026-01-17 13:27:09 +0500

    order controllers added for v1 and v2

---
+++ aspnet-webapi-versioning/Controllers/v1/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_webapi_versioning.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiversion}/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        public string Get()
+        {
+            return "Order information from API v1";
+        }
+    }
+}
---
+++ aspnet-webapi-versioning/Controllers/v2/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_webapi_versioning.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiversion}/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [MapToApiVersion("2.0")]
+        [HttpGet]
+        public string Get()
+        {
+            return "Order information from API v2";
+        }
+    }
+}
